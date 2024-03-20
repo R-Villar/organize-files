2e6cf947c3082ab672229209ef66017adcd5f67b
Remberto Villar <rembertovillar@gmail.com>
2024-03-20 13:45:04 -0400

    seperate files.

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import os
 from pathlib import Path
 
-SUBDIR = {
-    "DOCUMENTS": [".pdf", ".docx", ".txt"],
-    "AUDIO": [".m4a", ".m4b", ".mp3"],
-    "IMAGES": [".jpg", ".jpeg", ".png", ".heic", ".svg", ".webp"],
-    "ZIP": [".zip", ".rar"],
-}
+from subdirectory import SUBDIR
 
 
 def pickDirectory(value):
---
+++ subdirectory.py
@@ -0,0 +1,6 @@
+SUBDIR = {
+    "DOCUMENTS": [".pdf", ".docx", ".txt"],
+    "AUDIO": [".m4a", ".m4b", ".mp3"],
+    "IMAGES": [".jpg", ".jpeg", ".png", ".heic", ".svg", ".webp"],
+    "ZIP": [".zip", ".rar"],
+}
