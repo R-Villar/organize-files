859f4989d7c0e307b836a26eb7bd2c8c3a57584b
Remberto Villar <rembertovillar@gmail.com>
2024-03-29 12:28:05 -0400

    renamed sub dirs.

--- subdirectory.py
+++ subdirectory.py
@@ -1,6 +1,6 @@
 SUBDIR = {
-    "DOCUMENTS": [".pdf", ".docx", ".txt"],
-    "AUDIO": [".m4a", ".m4b", ".mp3"],
-    "IMAGES": [".jpg", ".jpeg", ".png", ".heic", ".svg", ".webp"],
-    "ZIP": [".zip", ".rar"],
+    "Documents": [".pdf", ".docx", ".txt"],
+    "Audio": [".m4a", ".m4b", ".mp3"],
+    "Images": [".jpg", ".jpeg", ".png", ".heic", ".svg", ".webp"],
+    "Zip files": [".zip", ".rar"],
 }
