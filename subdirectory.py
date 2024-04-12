470fc7b903ec6adf7db3b3b84214580f51123a59
Remberto Villar <rembertovillar@gmail.com>
2024-04-12 11:41:42 -0400

    add docstring.

--- subdirectory.py
+++ subdirectory.py
@@ -1,3 +1,11 @@
+"""
+If you would like to add more subdirectories, you can add them here.
+The key is the name of the subdirectory and the value is a list of file extensions.
+You can add as many subdirectories as you like.
+Make sure to add the file extensions in the format ".extension", as well as,
+the file extensions that commonly appear in your system.
+"""
+
 SUBDIR = {
     "Documents": [".pdf", ".docx", ".txt"],
     "Audio": [".m4a", ".m4b", ".mp3"],
